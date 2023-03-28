c179dd3697c230cd9cf93547ebd76226d0dba9d4
ishaq-p <sultan.ishaq.321@gmail.com>
2023-03-28 13:04:39 +0300

    added the input parts (username and passcode)

--- login.py
+++ login.py
@@ -1,12 +1,15 @@
 import numpy as np
 import re
 import pandas as pd
+from getpass import getpass
 
 class Login:
 
     def __init__(self,username,password):
         self.username = username
         self.password = password
+        self.username = input("please enter your username: ").strip() # strip for removing spaces
+        self.password = getpass("please enter your password: ")
 
     def __str__(self):
         return self.username
@@ -25,4 +28,3 @@ class Login:
 
     def decryption(self):
         pass
-    
