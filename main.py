e792ca610cd1987ddc561e2f497c0e8a05a2c203
ishaq-p <sultan.ishaq.321@gmail.com>
2023-04-08 04:00:31 +0300

    aded stuff in main to login (but still some error)

--- main.py
+++ main.py
@@ -12,8 +12,52 @@ import login as LI
 
 user = input("do you have an account? (y/n): ").strip()
 if user == "y":
-    user = LI.Login("","")
-    print(user)
+    username = input("please enter your username: ").strip()
+    password = getpass("please enter your password: ")
+
+    user = LI.Login(username,password)
+    bol, msg = user.search_for_existence()
+
+    if not bol:
+        print(msg)
+        ask_ = input("would you like to change your password? (y/n): ").strip()
+
+        if ask_ == "y":
+            bol1, msg1 = user.user_verification_for_password_change()
+
+            if bol1:
+                new_password = getpass("please enter your new password(1): ")
+                bol2, msg2 = user.password_context_validity(new_password)
+
+                while bol2==False:
+                    print(msg2)
+                    new_password = getpass("please enter your new password(2): ")
+                    bol2, msg2 = user.password_context_validity(new_password)
+
+                while new_password != input("please re-enter your new password(3): "):
+                    print("the passwords do not match")
+                    new_password = getpass("please enter your new password(5): ")
+                    while not user.password_context_validity(new_password)[0]:
+                        new_password = getpass("please enter your new password(6): ")
+                    
+                user.password = new_password
+                user.database()
+                print(user)
+
+            else:
+                print(msg)
+
+        else:
+            print("good bye")
+
+    else:
+        print("\n",msg,"\n")
+        print(user)
+
+
+
+
+
 elif user == "n":
         Full_Name = input("please enter your Full Name: ").strip()
         email = input("please enter your email: ").strip()
